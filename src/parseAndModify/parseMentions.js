17e83fa4d97023992f543418d7a14506418096c2
Francesco Giordano <francesco.giordano@buildo.io>
2017-07-21 14:32:20 +0200

    fix

--- src/parseAndModify/parseMentions.js
+++ src/parseAndModify/parseMentions.js
@@ -4,7 +4,7 @@ const inlineCodePlaceholder = '5lreaRWKv6LN';
 const inlineCodePlaceholderRe = new RegExp(inlineCodePlaceholder, 'g');
 const blockCodePlaceholder = 'kRxOO2NL9fuN';
 const blockCodePlaceholderRe = new RegExp(blockCodePlaceholder, 'g');
-const mentionRe = /([\s\n\r])@([A-Za-z0-9_.]+)/g;
+const mentionRe = /([\s\n\r])@([A-Za-z0-9_.`]+)/g;
 
 module.exports = (articleString) => {
   const inlineCodes = [];
@@ -13,6 +13,6 @@ module.exports = (articleString) => {
     .replace(inlineCodeRe, (_, code) => { inlineCodes.push(code); return inlineCodePlaceholder; })
     .replace(blockCodeRe, (_, code) => { blockCodes.push(code); return blockCodePlaceholder; })
     .replace(mentionRe, (_, s, username) => `${s}[@${username}](https://medium.com/@${username})` )
-    .replace(inlineCodePlaceholderRe, inlineCodes.shift())
-    .replace(blockCodePlaceholderRe, blockCodes.shift());
+    .replace(inlineCodePlaceholderRe, () => inlineCodes.shift())
+    .replace(blockCodePlaceholderRe, () => blockCodes.shift());
 };
